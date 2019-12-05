6f85ae954ec77975f1986d5855151ee8b3bd36f6
Ben Luginbuhl <ben.luginbuhl@gmail.com>
2019-12-05 12:01:09 -0800

    Contact info

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,15 +4,29 @@ class Header extends Component {
   render() {
       return(
         <header>
-            <hr align="center"></hr>
+            <hr />
             <span id="vitae-label">
                 <div><i className="fa fa-angle-double-right"></i></div>
                 <div id="vitae-text">Curriculum Vitae</div>
                 <div><i className="fa fa-angle-double-left"></i></div>
             </span>
-            <hr></hr>
-            <div id="header-name">Benjamin R. Luginbuhl</div>
-
+            <hr />
+            <div id="header-name">John Quincy Adams</div>
+            <hr />
+            <div id="contact-info">
+                <div className="contact-item">
+                    <div><i className="fa fa-envelope"></i></div>
+                    <div className="contact-text">johnqadams@email.com</div>
+                </div>
+                <div className="contact-item">
+                    <div><i className="fa fa-linkedin"></i></div>
+                    <div className="contact-text">linkedin.com/johnadams</div>
+                </div>
+                <div className="contact-item">
+                    <div><i className="fa fa-globe"></i></div>
+                    <div className="contact-text">johnadams.com</div>
+                </div>
+            </div>
         </header>
       );
   }
