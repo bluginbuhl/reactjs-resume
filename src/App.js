276d08a69c656adda150fe9600183e51654afbf6
Ben Luginbuhl <ben.luginbuhl@gmail.com>
2019-12-05 12:08:22 -0800

    Add about section

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
-import Header from './Components/Header'
+import Header from './Components/Header';
+import About from './Components/About';
 
 import './App.css';
 
@@ -8,6 +9,7 @@ function App() {
   return (
     <div className="page">
       <Header />
+      <About />
     </div>
     
   );
